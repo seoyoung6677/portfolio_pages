5a489e1e5bc92b2af6c1651059bce15abeae5caf
yunseoyoung <seoyoung6695@gmail.com>
2025-07-17 17:57:22 +0900

    마지막 스와이퍼, 호버 빼고 마무리

--- scripts/index.js
+++ scripts/index.js
@@ -3,3 +3,31 @@ const wrap =new Swiper('#wrap',{
     direction:'vertical',
     mousewheel:true,
 })
+
+document.querySelectorAll('nav a').forEach((obj)=>{
+    obj.addEventListener('click',(e)=>{
+        e.preventDefault(); //a의 href기본기능 막기
+        const slideNum = Number(obj.dataset.slide);
+        wrap.slideTo(slideNum, 1000);
+});
+});
+
+//sns 디자인
+const sns = new Swiper('#sns_swiper',{
+    slidesPerView:4,
+    autoplay:{delay:0,},
+    speed:4000,
+    loop:true,
+    spaceBetween:20,
+})
+
+
+// detail 디자인
+const detailDesign = document.querySelectorAll('#detail_swiper .swiper-slide ')
+const detail = new Swiper('#detail_swiper',{
+    slidesPerView:4,
+    autoplay:{delay:0,},
+    speed:4000,
+    loop:true,
+    spaceBetween:20,
+})
