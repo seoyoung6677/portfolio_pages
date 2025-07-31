36c4799fa56b48c247b131a5022ae2df7db4fd64
yunseoyoung <seoyoung6695@gmail.com>
2025-08-01 02:02:50 +0900

    그래픽 디자인행 완료

--- scripts/index.js
+++ scripts/index.js
@@ -13,7 +13,7 @@ document.querySelectorAll('nav a').forEach((obj)=>{
 });
 
 //sns 디자인
-const sns = new Swiper('#sns_swiper',{
+const sns = new Swiper('.sns_swiper',{
     slidesPerView:4,
     autoplay:{delay:0,},
     speed:4000,
@@ -24,29 +24,31 @@ const sns = new Swiper('#sns_swiper',{
 
 // detail 디자인
 
-const poster = new Swiper('#poster_swiper',{
+const poster = new Swiper('.poster_swiper',{
     slidesPerView:3,
     autoplay:{delay:0,},
     speed:4000,
     loop:true,
     spaceBetween:20,
 })
-const detail = new Swiper('#detail_design_swiper',{
+const detail = new Swiper('.detail_design_swiper',{
     slidesPerView:1,
     autoplay:{delay:0,},
     speed:5000,
     loop:true,
-    spaceBetween:20,
+    
 })
 
-const designProject = document.querySelectorAll('#design_containers .swiper-slide');
-const popup = document.querySelector('.popup_bg');
+//SNS 프로젝트 클릭 시 팝업실행(클릭한 이미지가 팝업 이미지로 교체)
+const snsProject = document.querySelectorAll('#sns_swiper .swiper-slide ')
+const popup= document.querySelector('.popup_bg')
+console.log(snsProject,popup);
 popup.addEventListener('click',()=>{popup.style.display='none'})
 
-for(let design of designProject){
+for(let sns of snsProject){
     sns.addEventListener('click',()=>{
         popup.style.display ='block';
-        popup.children[0].children[0].src = design.childern[0].src;
+        popup.children[0].children[0].src = sns.children[0].src;
         //팝업 실행 시 전체 수직 Swiper 스크롤 가능 막기
         wrap.mousewheel.disable(); //스크롤 풀기enable()
         
